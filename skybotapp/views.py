b405b8e6262cb910ae5bd761858970b3ba6136bd
melisgul <gulenay@itu.edu.tr>
2017-05-17 13:51:32 +0300

    access token

--- skybotapp/views.py
+++ skybotapp/views.py
@@ -14,7 +14,7 @@ import copy, datetime
 
 
 def post_facebook_message(fbid, recevied_message):           
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAASfh0TDd8cBAHBMfkWQGAexatTOup01lZCXtUJ5CF5Imr5b7MeQu30v6TnEzQmvoJF9MZBzkoZBdhLaVcCSY2BtPivUNJh7pic5vfEA13qDr3TRQLuHn8aKpKZAip4X2QHqhBTa7XQNGPnII1cqNMP46gAaRYMzHHSnZA4NZCAwZDZD' 
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAbIJorEeDwBAHWijUBAwU2ZCCxrwEufDvdfRVk9ZBhEoIHoDqbiMqtr1ZBZBaLOwQeiRZBrTd5ZBHLHlUgDKCmzd5hAnZBCcRkAKuEuCEZCHUyNMwO4rbn5ACzOyNNBN1DPdKexuhupJDtn3NlIuZBZCLB7hPF0Am4ckjfjCLGdFWfgZDZD' 
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
